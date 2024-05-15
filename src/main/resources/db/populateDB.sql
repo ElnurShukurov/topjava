7d93b71056c2bbf0ec0e0a92100d35366135329b
ElnurShukurov <shukurovs.e@gmail.com>
2024-05-15 20:45:52 +0400

    Add meals table and implement JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM users;
+DELETE
+FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +14,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2024-05-03 12:00:00', 'Breakfast', 400, 100000),
+       ('2024-05-03 18:00:00', 'Dinner', 600, 100000),
+       ('2024-05-03 08:00:00', 'Breakfast', 500, 100001),
+       ('2024-05-03 14:00:00', 'Lunch', 700, 100001);
